5ca98846ba8b4d7be69a1700e4bb93b745e8688b
Daniele Casadio <godhouse78@gmail.com>
2017-10-11 15:03:07 +0200

    Impostazione salvataggio settaggi

---
+++ js/admin.js
@@ -0,0 +1,40 @@
+
+(function() {
+	if (!OCA.RawPreview) {
+		/**
+		 * Namespace for the files app
+		 * @namespace OCA.RawPreview
+		 */
+		OCA.RawPreview = {};
+	}
+
+	/**
+	 * @namespace OCA.RawPreview.Admin
+	 */
+	OCA.RawPreview.Admin = {
+		initialize: function() {
+			$('#submitExiftoolPosition').on('click', _.bind(this._onClickSubmitExifTool, this));
+		},
+
+		_onClickSubmitExifTool: function () {
+			OC.msg.startSaving('#exiftoolPositionSettingsMsg');
+
+			var request = $.ajax({
+				url: OC.generateUrl('/apps/rawpreview/settings/setExif'),
+				type: 'POST',
+				data: {
+					maxUploadSize: $('#maxUploadSize').val()
+				}
+			});
+
+			request.done(function (data) {
+				$('#exiftoolPosition').val(data.exiftoolPosition);
+				OC.msg.finishedSuccess('#exiftoolPositionSettingsMsg', 'Saved');
+			});
+
+			request.fail(function () {
+				OC.msg.finishedError('#exiftoolPositionSettingsMsg', 'Error');
+			});
+		}
+	}
+})();
--- templates/admin.php
+++ templates/admin.php
@@ -1,3 +1,5 @@
+<?php OCP\Util::addScript('rawpreview', 'admin'); ?>
+
 <div class="section">
     <h2><?php p($l->t('Raw Preview')); ?></h2>
     <label for="exiftoolPosition"><?php p($l->t( 'Position of exiftool command' )); ?> </label>
@@ -6,5 +8,4 @@
     <input type="text" name='exiftoolPosition' id="exiftoolPosition" value='<?php p($_['exiftoolPosition']) ?>' />
     <input type="hidden" value="<?php p($_['requesttoken']); ?>" name="requesttoken" />
     <input type="submit" id="submitExiftoolPosition" value="<?php p($l->t( 'Save' )); ?>"/>
-    <p><em><?php p($l->t('With PHP-FPM it might take 5 minutes for changes to be applied.')); ?></em></p>
 </div>
