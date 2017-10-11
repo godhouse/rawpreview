7d61d6e3c24da97b26aa98f877502745b0c920a3
Daniele Casadio <godhouse78@gmail.com>
2017-10-11 11:35:19 +0200

    Removed l10n

--- templates/admin.php
+++ templates/admin.php
@@ -4,9 +4,9 @@
 ?>
 
 <div id="rawpreview_settings" class="section">
-        <h2><?php p($l->t('Raw Preview')); ?></h2>
+        <h2>Raw Preview</h2>
 
-        <h3><?php p($l->t('Things to define')); ?></h3>
+        <h3>Exif Tool</h3>
         <p>
             <input type="text" class="rawpreview_exiftool" id="rawpreview_exiftool_position" name="rawpreview_exiftool_position" value="<?php p($_['exiftool_position']) ?>">
         </p>
