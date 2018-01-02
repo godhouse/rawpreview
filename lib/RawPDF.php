c6443137c1a0f52e513a63109709e8a0d343940b
Daniele <godhouse78@gmail.com>
2018-01-02 19:56:23 +0100

    Added config for ffmpeg & libreoffice

--- lib/RawPDF.php
+++ lib/RawPDF.php
@@ -17,7 +17,7 @@ class RawPDF implements IProvider {
         $absPath = $fileview->toTmpFile($path);
         $tmpDir = \OC::$server->getTempManager()->getTempBaseDir();
 
-        $folder = \OC::$server->getAppDataDir('rawpreview');
+        $folder = \OC::$server->getRootFolder();
         \OCP\Util::writeLog('core', "folder " . $folder, \OCP\Util::ERROR);
         //create imagick object from pdf
         $pdfPreview = null;
